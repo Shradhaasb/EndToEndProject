2f17e225d1c8779027e22a27b4ede2b002cdd177
Shradha <Shradha@Shradha>
2024-07-22 17:50:21 +0530

    Prop File added

--- src/main/java/BaseLayer/BaseClass.java
+++ src/main/java/BaseLayer/BaseClass.java
@@ -1,6 +1,8 @@
 package BaseLayer;
 
+import java.io.FileInputStream;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,8 +10,29 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class BaseClass {
+	
+	
 
 	public static WebDriver driver;
+	public   Properties prop ;
+	
+	
+	public BaseClass() {
+		
+		 prop = new Properties() ;
+			String path=System.getProperty("user.dir")+"/src/main/java/Configuration/Config.properties";
+			
+			FileInputStream fis;
+			try {
+				fis = new FileInputStream(path);
+				prop.load(fis);
+			} catch (Exception e) {
+				
+				e.getLocalizedMessage();
+			}
+			
+		}
+	
 
 	public static void initialization(String browserName) {
 
@@ -38,4 +61,5 @@ public class BaseClass {
 
 	}
 
+	
 }
